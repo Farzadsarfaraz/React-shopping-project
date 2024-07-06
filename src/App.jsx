1283bde5137d182314ee72d332ffa8ee9c8b40dd
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-06 17:05:33 +0200

    new-photos

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoggingSignup from "./Pages/LoggingSignup";
 import Footer from "./components/Footer/Footer";
-import men_banner from './components/assets/pic12.png'
-import women_banner from './components/assets/pic13.png'
-import kid_banner from './components/assets/kid (5).webp'
+import men_banner from './components/assets/men1.png'
+import women_banner from './components/assets/pic16.png'
+import kid_banner from './components/assets/kid1.png'
 
 function App() {
   return (
@@ -21,11 +21,11 @@ function App() {
           <Route path="/mens" element={<ShopCategory banner={men_banner} category="mens"  />} />
           <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />}/>
           <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
-          <Route path="/product" element={<Product />}>
-            <Route path=":Product" element={<Product />}/>
+          <Route path="/product" element={<Product/>}>
+            <Route path=":ProductId" element={<Product/>}/>
           </Route>
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoggingSignup />}/>
+          <Route path="/login" element={<LoggingSignup/>}/>
         </Routes>
         <Footer/>
       </Router>
--- src/Pages/LoggingSignup.jsx
+++ src/Pages/LoggingSignup.jsx
@@ -7,14 +7,14 @@ const LoggingSignup = () => {
       <h1>Sign UP</h1>
       <div className="loginsignup-field">
         <input type="text" placeholder="Your name"/>
-        <input type="email" placeholder="Eamil Address"/>
+        <input type="email" placeholder="Email Address"/>
         <input type="Password" placeholder="Password"/>
       </div>
       <button>Continue</button>
       <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
       <div className="loginsignup-agree">
         <input type="checkbox" name="" id=""/>
-        <p>By continuing, i agree to terms of use & privacy policy</p>
+        <p>I agree to terms of use & privacy policy</p>
       </div>  
     </div>
     </div>
