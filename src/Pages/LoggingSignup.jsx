1a7451b78624f1090e78915ae42aa37ab9022fa4
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-08 02:07:52 +0200

    login

--- src/Pages/LoggingSignup.jsx
+++ src/Pages/LoggingSignup.jsx
@@ -1,20 +1,43 @@
 
+import { useState } from 'react'
 import './CSS/LoggingSignup.css'
 const LoggingSignup = () => {
+
+const[state, setState] = useState("Sign Up")
+const [formatData, setFormatData] = useState({
+  username:"",
+  password: "",
+  email:""
+})
+const changeHandler =(e)=>{
+  setFormatData({...formatData, [e.target.name]:e.target.value})
+
+}
+
+const login = async() =>{
+  console.log(formatData)
+
+  
+}
+const signup = async() =>{
+
+
+}
+
   return (
     <div className="loginsignup">
       <div className='loginsignup-container'>
-      <h1>Sign UP</h1>
+      <h1>{state}</h1>
       <div className="loginsignup-field">
-        <input type="text" placeholder="Your name"/>
-        <input type="email" placeholder="Email Address"/>
-        <input type="Password" placeholder="Password"/>
+        
+        {state==="Sign Up"? <input name='username' value={formatData.username} type="text" onChange={changeHandler} placeholder="Your name"/>:<></>}
+        <input name='email' value={formatData.email} onChange={changeHandler} type="email" placeholder="Email Address"/>
+        <input name='password' value={formatData.password} onChange={changeHandler} type="Password" placeholder="Password"/>
       </div>
-      <button>Continue</button>
-      <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+      <button onClick={() =>{state ==="login"? login(): signup()}}>Continue</button>
+      {state === "Sign Up"?<p className="loginsignup-login">Already have an account? <span onClick={() =>setState("Login")}>Login here</span></p>: <p className="loginsignup-login">Create an account <span onClick={()=>setState("Sign Up")}>Click here</span></p> }
       <div className="loginsignup-agree">
-        <input type="checkbox" name="" id=""/>
-        <p>I agree to terms of use & privacy policy</p>
+        <label name =""><p> <input type="checkbox" name="" id=""/> I agree to terms of use & privacy policy</p></label>
       </div>  
     </div>
     </div>
