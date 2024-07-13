1ec86889e7c37764d0fce8cfe563b94d0d1e4a93
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-13 20:29:44 +0200

    n

--- src/components/Bootstrap/Bootstrap.jsx
+++ src/components/Bootstrap/Bootstrap.jsx
@@ -13,7 +13,7 @@ const ImageSlider = ({ images }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2500,
     cssEase: "linear",
   };
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,18 +2,16 @@ import "./Hero.css";
 import HomeImage from "./../assets/prof.webp";
 import logo from "./../assets/rightLogo.gif";
 import ImageSlider from "../Bootstrap/Bootstrap";
-import photo from "../assets/prof.webp";
 import photo1 from "../assets/kid1.png";
 import photo5 from "../assets/Cloth-123.01.webp";
 import photo6 from "../assets/png (1).png";
-import photo7 from "../assets/png (2).png";
 import photo8 from "../assets/png (3).png";
 import photo9 from "../assets/png (4).png";
 import photo10 from "../assets/png (5).png";
 import photo11 from "../assets/Cloth-128.1.webp";
 
 const Hero = () => {
-  const images = [photo, photo1, photo5, photo6, photo7, photo8, photo9, photo10, photo11];
+  const images = [photo1, photo5, photo6, photo8, photo9, photo10, photo11];
 
   return (
       <div className="hero">
