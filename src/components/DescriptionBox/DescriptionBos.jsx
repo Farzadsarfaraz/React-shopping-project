013748c1acaaa0b66929c3d3cec76e257614c2a8
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-18 23:42:58 +0200

    responsive

--- src/components/DescriptionBox/DescriptionBos.jsx
+++ src/components/DescriptionBox/DescriptionBos.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import './DescriptionBox.css'
+import React from "react";
+import "./DescriptionBox.css";
 
 export const DescriptionBos = () => {
   return (
-    <div className='descriptionbox'>
-        <div className="descriptionbox-navigator">
-            <div className="descriptionbox-nav-box">Description</div>
-            <div className="descriptionbox-nav-box fade">Reviews(122)</div>
-        </div>
-        <div className="descriptionbox-description">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam soluta, sequi dolores officia aut dolorem quidem. Similique et sint in amet, eos error sit reiciendis, iure perspiciatis aperiam incidunt vero!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam soluta, sequi dolores officia aut dolorem quidem. Similique et sint in amet, eos error sit reiciendis, iure perspiciatis aperiam incidunt vero!</p>
-        </div>
-
-
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+          soluta, sequi dolores officia aut dolorem quidem. Similique et sint in
+          amet, eos error sit reiciendis, iure perspiciatis aperiam incidunt
+          vero!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+          soluta, sequi dolores officia aut dolorem quidem. Similique et sint in
+          amet, eos error sit reiciendis, iure perspiciatis aperiam incidunt
+          vero!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
+import React from "react";
 import "./Footer.css";
 import footer_logo from "../assets/logo.png";
 import instagram_icon from "../assets/instagram.png";
 import whatsapp_icon from "../assets/whatsapp.png";
 
 const Footer = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "3cbafd03-c31f-431b-8ec7-cf0383fb56f8");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className="Wrapper">
     <div className="footer">
@@ -17,6 +44,14 @@ const Footer = () => {
         <li>About</li>
         <li>Help & Contact</li>
       </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} alt="" width="30px" />
+        </div>
+        <div className="footer-icons-container">
+          <img src={whatsapp_icon} alt="" width="30px" />
+        </div>
+      </div>
     </div>
     <div className="footer2">
       <h1>USEFUL LINKS</h1>
@@ -26,19 +61,19 @@ const Footer = () => {
       <p>Refund and Returns Policy</p>
       <p>FAQs</p>
     </div>
-    <div className="footer2">
-      <h1>Our partner</h1>
-      <p>Amazone</p>
-      <hr/>
-      <div className="footer-social-icon">
-        <div className="footer-icons-container">
-          <img src={instagram_icon} alt="" width="30px" />
-        </div>
-        <div className="footer-icons-container">
-          <img src={whatsapp_icon} alt="" width="30px" />
-        </div>
+      <div className="contact">
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+            <label>Your name</label>
+            <input type="text" name="name" placeholder="Enter your name" required/>
+            <label>Your Number</label>
+            <input type="tel" name="phone" placeholder="Enter your phone number" required/>
+            <label>Write your message</label>
+            <textarea name="message" rows="6" placeholder="Enter your message"></textarea>
+            <button type="submit" className="btn dark-btn"> Submit</button>
+        </form>
+        <span>{result}</span>
       </div>
-
     </div>
     </div>
   );
